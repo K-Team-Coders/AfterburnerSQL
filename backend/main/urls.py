b80e9deadc2c87f3ef805f4f344dfe24f39149fe
qTemio <zhitihin.artem1441@yandex.ru>
2022-09-24 18:30:12 +0300

    not ready

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('load_file/', countTableUsability.as_view()),
-    # path('')
+    path('load_file_tables/', countTableUsability.as_view()),
+    path('load_file_user/', countUserActivity.as_view())
 ]
