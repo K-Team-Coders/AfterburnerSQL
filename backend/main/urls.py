484c000ebed5f6d3a6390cf77258cbb92c8ede19
qTemio <zhitihin.artem1441@yandex.ru>
2022-09-25 02:37:50 +0300

    added models + api to query prediction

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -5,5 +5,7 @@ from .views import *
 urlpatterns = [
     path('load_file_tables/', countTableUsability.as_view()),
     path('load_file_users/', countUserActivity.as_view()),
-    path('predict_query_time_execution/<str:query>/', predictQueryResponseTime.as_view())
+    path('predict_query_time_execution/<str:query>/', predictQueryResponseTime.as_view()),
+    path('predict_query_time_execution_operators/<str:query>/', predictQueryResponseTimeOperatorsCount.as_view()),
+    path('predictQueryResponseTimeDesicionTree/<str:query>/', predictQueryResponseTimeDesicionTree.as_view())
 ]
