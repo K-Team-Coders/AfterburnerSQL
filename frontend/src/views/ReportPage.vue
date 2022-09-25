3df50237d4527a2841af767f7affbffc51e70676
Your Carma <y0urcarma@yandex.ru>
2022-09-25 11:27:45 +0300

    TrashDeleted

--- frontend/src/views/ReportPage.vue
+++ frontend/src/views/ReportPage.vue
@@ -1,27 +1,22 @@
 <template>
-<div class="bg-gray-100"><div class=" h-screen shadow-inner">
-    <Navbar></Navbar>
-    <div class="px-64">
-        <div class = "bg-opacity-10 backdrop-blur bg-white h-screen shadow-2xl">
+    <div class="bg-gray-100">
+        <div class=" h-screen shadow-inner">
+            <Navbar></Navbar>
+            <div class="px-64">
+                <div class="bg-opacity-10 backdrop-blur bg-white h-screen shadow-2xl">
+
+                </div>
+            </div>
             
         </div>
     </div>
-    <div class="bg-gray-100 h-screen -mt-16 shadow-inner">
-        <div class="px-64">
-        <div class = "bg-opacity-10 backdrop-blur bg-white h-screen shadow-2xl">
-            
-        </div>
-    </div>
-    </div>
-</div>
-</div>
 
 </template>
 
 <script>
 import Navbar from "../components/Navbar.vue";
-export default{
-    components: {Navbar}
+export default {
+    components: { Navbar }
 }
 </script>
 
