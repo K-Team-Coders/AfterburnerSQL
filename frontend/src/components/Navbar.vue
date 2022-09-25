4bbbe726b6ee31a34beee784d0444c0b0954d831
Your Carma <y0urcarma@yandex.ru>
2022-09-25 10:45:37 +0300

    LogoAdded

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -4,7 +4,7 @@
           <div class="container px-6 h-16 flex justify-between items-center lg:items-stretch mx-auto">
               <div class="flex items-center">
                   <div tabindex="0" class="focus:outline-none mr-10 flex items-center">
-                      <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/black-left-aligned-with-search-bar-icons-svg1.svg" alt="north" />
+                      <img src="../assets/afterburner.png" alt="north" />
                       <h3 class="text-base text-white font-bold tracking-normal leading-tight ml-3 hidden lg:block"><a href="#/">AfterburnerSQL</a></h3>
                   </div>    
                   <div tabindex="0" class="focus:outline-none mr-10 flex items-center">
