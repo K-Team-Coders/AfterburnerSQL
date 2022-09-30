44ebb467bbdf7d6b5da6351b17fcaf41e0a4f615
qTemio <zhitihin.artem1441@yandex.ru>
2022-09-30 15:19:02 +0300

    cleared lfs

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -58,6 +58,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8081',
+    'http://localhost:8080',
     "https://domain.com",
     "https://api.domain.com",
     "http://localhost:8080",
